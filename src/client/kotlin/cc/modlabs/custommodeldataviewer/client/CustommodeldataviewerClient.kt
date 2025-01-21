bf1ba8634f99c97af14f285ee749255cddb7c056
CoasterFreakDE <mleaked@gmail.com>
2025-01-21 19:45:05 +0100

    feat: Custom CMDV Tab Item

--- src/client/kotlin/cc/modlabs/custommodeldataviewer/client/CustommodeldataviewerClient.kt
+++ src/client/kotlin/cc/modlabs/custommodeldataviewer/client/CustommodeldataviewerClient.kt
@@ -3,6 +3,8 @@ package cc.modlabs.custommodeldataviewer.client
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper
+import net.minecraft.component.DataComponentTypes
+import net.minecraft.component.type.CustomModelDataComponent
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
@@ -26,9 +28,16 @@ class CustommodeldataviewerClient : ClientModInitializer {
         val CUSTOM_ITEM_GROUP_KEY: RegistryKey<ItemGroup> =
             RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(MOD_ID, "item_group"))
         val CUSTOM_ITEM_GROUP: ItemGroup = FabricItemGroup.builder()
-            .icon(Supplier { ItemStack(Items.PAPER) })
+            .icon(Supplier { getCMDVItem() })
             .displayName(Text.translatable("itemGroup.$MOD_ID"))
             .build()
+
+        private fun getCMDVItem(): ItemStack {
+            val item = ItemStack(Items.DEBUG_STICK)
+            val customModelData = CustomModelDataComponent(listOf(), listOf(), listOf("cmdv"), listOf())
+            item.set(DataComponentTypes.CUSTOM_MODEL_DATA, customModelData)
+            return item
+        }
     }
 
     override fun onInitializeClient() {
