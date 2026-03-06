b0f7138f80d2f1c90f7cf217b6d70fab6fb5d8d3
Liam Sage <mleaked@gmail.com>
2026-03-06 17:43:24 +0100

    mojang mappings

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ repositories {
 dependencies {
     // To change the versions see the gradle.properties file
     minecraft("com.mojang:minecraft:${stonecutter.current.version}")
-    mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}:v2")
+    mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
     modImplementation("net.fabricmc:fabric-language-kotlin:${project.property("kotlin_loader_version")}")
 
